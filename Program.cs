b74a57ca48f7799c4ffe5497f4551bc0f76a232c
Will Pearson <will@mcneel.com>
2020-05-21 14:48:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Rhino.FileIO;
+
+namespace Rhino3dmCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine(@"Usage: dotnet run hello_mesh.3dm");
+                return;
+            }
+
+            var path = args[0];
+            var doc = File3dm.Read(path);
+
+            Console.WriteLine($"{path} has {doc.Objects.Count} objects!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## rhino3dm .NET Core Example
+
+```commandline
+$ dotnet run hello_mesh.3dm
+```
